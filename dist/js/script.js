100b1257a1fe69b11699811504bacdb09d03d23d
nickolashka <xy4u74@yandex.ru>
2019-09-15 20:55:32 +0500

    compress img & make validation

--- dist/js/script.js
+++ dist/js/script.js
@@ -105,20 +105,20 @@ $(document).ready(function(){
     //Hamburger
 
     
-        const menu = document.querySelector('.header__menu'),
-        menuItem = document.querySelectorAll('.header__menu > li'),
-        hamburger = document.querySelector('.header__hamburger');
-    
-        hamburger.addEventListener('click', () => {
+    const menu = document.querySelector('.header__menu'),
+    menuItem = document.querySelectorAll('.header__menu > li'),
+    hamburger = document.querySelector('.header__hamburger');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('header__hamburger_active');
+        menu.classList.toggle('header__menu_active');
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
             hamburger.classList.toggle('header__hamburger_active');
             menu.classList.toggle('header__menu_active');
-        });
-    
-        menuItem.forEach(item => {
-            item.addEventListener('click', () => {
-                hamburger.classList.toggle('header__hamburger_active');
-                menu.classList.toggle('header__menu_active');
-            })
         })
+    })
 
   });
